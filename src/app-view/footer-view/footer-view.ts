e6e542c08d69bf87892cbaf01f8aea0ab1ec17e4
shoegaev <kshoegaev@gmail.com>
2024-09-15 17:56:09 +0300

    feat: add footer content, implement footer adaptivity

--- src/app-view/footer-view/footer-view.ts
+++ src/app-view/footer-view/footer-view.ts
@@ -1,6 +1,8 @@
 import { View } from "../../util/view";
-import { ElementParametrs } from "../../util/element-creator";
+import { ElementCreator, ElementParametrs } from "../../util/element-creator";
 import "./footer-style.scss";
+import rsIcon from "../../../public/assets/icons/rs_school.svg";
+import githubIcon from "../../../public/assets/icons/github-logo.svg";
 
 export class FooterView extends View {
   constructor() {
@@ -9,5 +11,40 @@ export class FooterView extends View {
       cssClasses: ["footer"],
     };
     super(FOOTER_PARAMS);
+    this.configureView();
+  }
+
+  private configureView(): void {
+    const rsSchoolLink = new ElementCreator({
+      tag: "a",
+      cssClasses: ["footer__rs-school-link"],
+      atributes: [
+        { name: "href", value: "https://rs.school/courses/javascript-ru" },
+      ],
+    });
+    const rsIconEl = new ElementCreator({
+      tag: "img",
+      cssClasses: ["footer__rs-school-icon"],
+      atributes: [{ name: "src", value: rsIcon }],
+    });
+    rsSchoolLink.apendInnerElements(rsIconEl);
+    const year = new ElementCreator({
+      tag: "span",
+      cssClasses: ["footer__year"],
+      textContent: "2024",
+    });
+    const githubLink = new ElementCreator({
+      tag: "a",
+      cssClasses: ["footer__github-link"],
+      atributes: [{ name: "href", value: "https://github.com/shoegaev" }],
+    });
+    const githubIconEl = new ElementCreator({
+      tag: "img",
+      cssClasses: ["footer__rs-school-icon"],
+      atributes: [{ name: "src", value: githubIcon }],
+    });
+    githubLink.apendInnerElements(githubIconEl);
+
+    this.viewCreator.apendInnerElements(rsSchoolLink, year, githubLink);
   }
 }
